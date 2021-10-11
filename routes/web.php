01d7af06f4ad3ef00b42d7cf825822ab593e699a
ibrahima411 <49150724+ibrahima411@users.noreply.github.com>
2021-10-11 16:15:18 -0700

    first

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\HomeController;
 
 /*
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
